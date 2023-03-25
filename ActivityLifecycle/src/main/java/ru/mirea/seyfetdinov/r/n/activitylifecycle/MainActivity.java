b693000c7efb0e9d3a8606bf8167d6ad0e0b1666
Пользователь <123rin12320@gmail.com>
2023-03-25 20:08:13 +0300

    init

---
+++ ActivityLifecycle/src/main/java/ru/mirea/seyfetdinov/r/n/activitylifecycle/MainActivity.java
@@ -0,0 +1,89 @@
+package ru.mirea.seyfetdinov.r.n.activitylifecycle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName().toString();
+
+    private EditText editText;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editTextTextPersonName);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String text = savedInstanceState.getString("data_value");
+        Log.d(TAG, "onRestoreInstanceState");
+        if (text != null) editText.setText(text);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.d(TAG, "onSaveInstanceState");
+        outState.putString("data_value", editText.getText().toString());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    public void invokeSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class); //Создаем намерение
+        intent.putExtra("key", editText.getText().toString());
+        startActivity(intent);
+    }
+
+    private void showInfo(String text){
+
+    }
+}
